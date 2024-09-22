86f5f8557a2a03e9235c644715e0d4851033c864
indeedornot <gravitybartek@gmail.com>
2024-09-22 11:39:09 +0200

    Add navbar twig helper

---
+++ src/Services/Navigation/NavigationExtension.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Services\Navigation;
+
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class NavigationExtension extends AbstractExtension
+{
+    public function __construct(
+        private Security        $security,
+        private RouterInterface $router,
+        private RequestStack    $stack,
+        private NavRouteFactory $routes
+    )
+    {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('getNavLinks', [$this, 'getNavLinks']),
+            new TwigFunction('isCurrentRoute', [$this, 'isCurrentRoute'])
+        ];
+    }
+
+    /** @return array<string, NavRoute> */
+    public function getNavLinks(): array
+    {
+        $isLoggedIn = $this->security->getUser() !== null;
+        $routes = [
+            'list' => $this->routes->new(
+                key: 'app_list',
+                label: 'List',
+            ),
+            'login' => $this->routes->new(
+                key: 'app_login',
+                label: 'Log In',
+                enabled: !$isLoggedIn && !$this->isCurrentRoute('app_login')
+            ),
+            'logout' => $this->routes->new(
+                key: 'app_logout',
+                label: 'Log out',
+                enabled: $isLoggedIn && !$this->isCurrentRoute('app_logout')
+            )
+        ];
+
+        return $routes;
+    }
+
+    public function isCurrentRoute(string $routeName): bool
+    {
+        return $this->routes->isCurrentRoute($routeName);
+    }
+}
