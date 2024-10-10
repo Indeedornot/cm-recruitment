21599edcb76de24701cf6dfe8e8af011ca1d6a4d
Indeed <gravitybartek@gmail.com>
2024-10-10 08:39:25 +0200

    i18 and related field fixes in ClientApplicationType.php

--- src/Form/ClientApplicationType.php
+++ src/Form/ClientApplicationType.php
@@ -3,43 +3,57 @@
 namespace App\Form;
 
 use App\Entity\ClientApplication;
-use App\Entity\PostingQuestion;
+use App\Repository\PostingQuestionRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Event\PreSetDataEvent;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ClientApplicationType extends AbstractType
 {
     public function __construct(
         private Security $security,
+        private PostingQuestionRepository $postingQuestionRepository
     ) {
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        /** @var PostingQuestion[] $questions */
-        $questions = $options['questions'];
-
         $builder
-            ->add('questionnaire', QuestionnaireType::class)
-            ->add('answers', CollectionType::class, [
-                'entry_type' => PostingAnswerType::class,
-                'entry_options' => [
-                    'questions' => $questions
-                ],
-                'allow_add' => true,
-                'allow_delete' => true,
-                'by_reference' => false
+            ->add('questionnaire', QuestionnaireType::class, [
+                'label' => 'Podstawowy Kwestionariusz'
             ]);
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (PreSetDataEvent $event) {
+            $form = $event->getForm();
+            $clientApplication = $event->getData();
+
+            if (empty($clientApplication)) {
+                return;
+            }
+
+            $posting = $clientApplication->getPosting();
+            $questions = $posting->getQuestions();
+
+            foreach ($questions as $index => $question) {
+                $form->add('answers_' . $index, PostingAnswerType::class, [
+                    'mapped' => false,
+                    'allow_extra_fields' => true,
+                    'question' => $question,
+                    'label' => $question->getTitle(),
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => ClientApplication::class,
-            'client' => $this->security->getUser()
+            'client' => $this->security->getUser(),
+            'posting' => null,
         ]);
     }
 }
