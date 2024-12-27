4af10a3cc0d93f8b2240d1997701f041dfe834b4
Indeed <gravitybartek@gmail.com>
2024-12-27 16:54:48 +0100

    Add test superuser

--- src/Controller/Test/TestController.php
+++ src/Controller/Test/TestController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Test;
 use App\Controller\Base\BaseController;
 use App\Security\Entity\Admin;
 use App\Security\Entity\Client;
+use App\Security\Entity\UserRoles;
 use App\Security\Factory\UserFactory;
 use App\Security\Repository\UserRepository;
 use App\Security\Services\ExtendedSecurity;
@@ -13,6 +14,7 @@ use LogicException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Translation\TranslatableMessage;
 
 #[Route(path: '/test', name: 'app_test_')]
 class TestController extends BaseController
@@ -32,37 +34,39 @@ class TestController extends BaseController
     public function useTestAccount(
         #[MapQueryParameter] string $type
     ): Response {
-        $type = match (strtolower($type)) {
-            'admin' => Admin::class,
+        $type = strtolower($type);
+        $class = match (($type)) {
+            'admin', 'superadmin' => Admin::class,
             'client' => Client::class,
             default => null,
         };
-        if ($type === null) {
+        if ($class === null) {
             return $this->renderErrorPage('Invalid account type');
         }
 
-        $user = $this->getTestAccount($type);
+        $user = $this->getTestAccount($class, $type);
         $this->security->login($user);
 
         return $this->redirectToRoute('app_index_index');
     }
 
-    private function getTestAccount(string $type)
+    private function getTestAccount(string $class, string $type)
     {
-        $name = explode('\\', $type);
-        $name = end($name);
-        $email = $name . '@example.com';
-
+        $email = $type . '@example.com';
         $user = $this->userRepository->findOneBy(['email' => $email]);
         if ($user === null) {
-            $user = match ($type) {
+            $user = match ($class) {
                 Admin::class => $this->userFactory->createEmptyAdmin(),
                 Client::class => $this->userFactory->createEmptyClient(),
             };
 
-            $user->setEmail($email)->setName($name);
+            $user->setEmail($email)->setName($type);
             if ($user instanceof Admin) {
                 $user->setCreatedBy($user);
+
+                if ($type === 'superadmin') {
+                    $user->setRoles([...$user->getRoles(), UserRoles::SUPER_ADMIN->value]);
+                }
             }
 
             $user->setPassword($this->userFactory->hashPassword($user, 'password'));
