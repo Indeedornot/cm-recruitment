7bc3ae81bea8b9deb4e443bedf6f651d9706600b
Indeed <gravitybartek@gmail.com>
2025-01-06 15:24:12 +0100

    Fix global config close_date updating

--- src/EventListener/GlobalConfigListener.php
+++ src/EventListener/GlobalConfigListener.php
@@ -4,6 +4,7 @@ namespace App\EventListener;
 
 use App\Entity\GlobalConfig;
 use App\Repository\PostingRepository;
+use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
@@ -18,7 +19,7 @@ class GlobalConfigListener
     public function preUpdate(GlobalConfig $globalConfig, PreUpdateEventArgs $event): void
     {
         if ($globalConfig->getKey() === 'closing_date' && array_key_exists('value', $event->getEntityChangeSet())) {
-            $this->postingRepository->updateActiveClosingDates($globalConfig->getValue());
+            $this->postingRepository->updateActiveClosingDates(new DateTimeImmutable($globalConfig->getValue()));
         }
     }
 }
