a59dbb3015ec03150bd0de03f3e8be4ddf4b3697
Indeed <gravitybartek@gmail.com>
2025-01-06 21:57:37 +0100

    Add link to regulamin

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use App\Controller\Base\BaseController;
 use App\Services\Navigation\NavigationExtension;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Asset\Packages;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(name: 'app_index_')]
 class IndexController extends BaseController
 {
-    public function __construct(private readonly NavigationExtension $navigationExtension)
-    {
+    public function __construct(
+        private readonly NavigationExtension $navigationExtension,
+        private readonly Packages $assetExtension
+    ) {
     }
 
     #[Route(path: '/', name: 'index')]
@@ -23,5 +27,11 @@ class IndexController extends BaseController
         }
 
         return $this->render('pages/user/index.html.twig');
+    }
+
+    #[Route(path: '/regulamin', name: 'regulamin')]
+    public function regulamin(): Response
+    {
+        return $this->redirect($this->assetExtension->getUrl('build/images/regulamin_rekrutacji.pdf'));
     }
 }
