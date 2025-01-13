254674e1e78a21007b054843b58609058e22749e
Indeed <gravitybartek@gmail.com>
2025-01-13 18:42:32 +0100

    Form button fix

--- assets/js/hooks/submitButton.ts
+++ assets/js/hooks/submitButton.ts
@@ -1,27 +1,27 @@
-const disableSubmitButton = (e: Event) => {
-  const element = e.target as HTMLButtonElement;
-  const form = element.closest('form');
-  let submitButtons;
-  if (form) {
-    submitButtons = form.querySelectorAll("[data-hook^='submit-button']");
-  } else {
-    submitButtons = [element];
-  }
-  console.log(submitButtons);
-  submitButtons.forEach((button) => {
+const disableSubmitButton = (button: Element) => {
+  setTimeout(() => {
     button.classList.add('disabled');
-    if (button.tagName === 'a') {
+    if (button.tagName === 'A') {
       button.setAttribute('href', '#');
     } else {
       // @ts-ignore
       button.disabled = true;
     }
-  });
+  }, 0);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const submitButtons = document.querySelectorAll("[data-hook^='submit-button']");
   submitButtons.forEach((button) => {
-    button.addEventListener('click', (e) => disableSubmitButton(e));
+    const form = button.closest('form');
+    if (form) {
+      form.addEventListener('submit', (e) => {
+        for (let i = 0; i < submitButtons.length; i++) {
+          disableSubmitButton(submitButtons[i]);
+        }
+      });
+    } else {
+      button.addEventListener('click', (e) => disableSubmitButton(e.target as Element));
+    }
   });
 });
